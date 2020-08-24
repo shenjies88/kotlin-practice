3ff13b8fc49228635c9d617ad7d4a72c094d7cfe
shenjies88 <1961486254@qq.com>
2020-08-24 21:52:39 +0800

    feat: 切换黑暗主题

--- kotlin-practice-app/app/src/main/java/com/example/kotlin_practice_app/activity/MainActivity.kt
+++ kotlin-practice-app/app/src/main/java/com/example/kotlin_practice_app/activity/MainActivity.kt
@@ -2,11 +2,29 @@ package com.example.kotlin_practice_app.activity
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
 import com.example.kotlin_practice_app.R
+import com.google.android.material.appbar.MaterialToolbar
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var topAppBar: MaterialToolbar
+    private lateinit var drawerLayout: DrawerLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        topAppBar = findViewById(R.id.top_app_bar)
+        drawerLayout = findViewById(R.id.drawer_layout)
+
+        topAppBar.setNavigationOnClickListener {
+            if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+                drawerLayout.closeDrawer(GravityCompat.START)
+            } else {
+                drawerLayout.openDrawer(GravityCompat.START)
+            }
+        }
     }
 }
