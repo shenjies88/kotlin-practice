4a0031da30d28783c3f650896a6d483b75ac26dc
shenjies88 <1961486254@qq.com>
2020-08-25 12:19:21 +0800

    fix

--- kotlin-practice-app/app/src/main/java/com/example/kotlin_practice_app/activity/MainActivity.kt
+++ kotlin-practice-app/app/src/main/java/com/example/kotlin_practice_app/activity/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.example.kotlin_practice_app.activity
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
 import com.example.kotlin_practice_app.R
+import com.example.kotlin_practice_app.manager.TokenManager
 import com.google.android.material.appbar.MaterialToolbar
 
 
@@ -16,6 +19,13 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //如果没有token就启动LoginActivity
+        if (TokenManager.getToken().isNullOrBlank()) {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+            Toast.makeText(this, "登陆已失效，请重新进行登陆", Toast.LENGTH_SHORT).show()
+        }
 
         topAppBar = findViewById(R.id.top_app_bar)
         drawerLayout = findViewById(R.id.drawer_layout)
