eb7cbdfb12e1c5119efd0a34104518468db22a89
shenjies88 <1961486254@qq.com>
2020-08-26 19:46:49 +0800

    fix

--- kotlin-practice-app/app/src/main/java/com/example/kotlin_practice_app/client/BackendClient.kt
+++ kotlin-practice-app/app/src/main/java/com/example/kotlin_practice_app/client/BackendClient.kt
@@ -13,6 +13,12 @@ import okhttp3.Headers
 class BackendClient {
     companion object {
 
+        /**
+         * 注册
+         * @param account 账号
+         * @param pwd 密码
+         * @param nickname 昵称
+         */
         fun registered(account: String, pwd: String, nickname: String, callback: Callback) {
             val url = "$BASE_URL/app/authentication/registered"
             val requestBody = AppRegisteredReqVo(account, pwd, nickname)
@@ -20,12 +26,20 @@ class BackendClient {
 
         }
 
+        /**
+         * 登陆
+         * @param account 账号
+         * @param pwd 密码
+         */
         fun login(account: String, pwd: String, callback: Callback) {
             val url = "$BASE_URL/app/authentication/login"
             val requestBody = AppLoginReqVo(account, pwd)
             OkHttpUtil.asyPost(url, GsonUtil.toJson(requestBody), callback)
         }
 
+        /**
+         * 我的用户信息
+         */
         fun myInfo(callback: Callback) {
             val url = "$BASE_URL/app/user/my-info"
             OkHttpUtil.asyPost(url, Headers.headersOf(APP_TOKEN, TokenManager.getToken()), callback)
