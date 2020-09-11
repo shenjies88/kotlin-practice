e95b828fe0a11cf6311154eb5f3564d988128caa
shenjie <shenjie@yunhx.net>
2020-09-11 11:13:54 +0800

    fix

--- kotlin-practice-backend/src/main/kotlin/com/shenjies88/practice/kotlin_practice_backend/vo/PageReqVo.kt
+++ kotlin-practice-backend/src/main/kotlin/com/shenjies88/practice/kotlin_practice_backend/vo/PageReqVo.kt
@@ -1,10 +1,15 @@
 package com.shenjies88.practice.kotlin_practice_backend.vo
 
+import io.swagger.annotations.ApiModelProperty
+
 /**
  * @author shenjies88
  * @since 2020/8/23-7:48 PM
  */
-open class PageReqVo(private val size: Int = 20,private val num: Int = 1) {
+open class PageReqVo(
+        @ApiModelProperty("分页数") open val size: Int = 10,
+        @ApiModelProperty("分页") open val num: Int = 1) {
 
-    val offset: Int = (num - 1) * size
+    @ApiModelProperty(hidden = true)
+    private val offset: Int = (num - 1) * size
 }
---
+++ kotlin-practice-web/src/mixin/PageMixin.js
@@ -0,0 +1,10 @@
+export default {
+  data() {
+    return {
+      num: 1,
+      size: 10,
+      pageSizeList: [10, 20, 30, 40, 50],
+      total: 0
+    }
+  },
+}
